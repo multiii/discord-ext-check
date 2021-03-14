dc950d8f199b3e101e42ef658ba8942df94fee41
multi-yt76 <72256091+multi-yt76@users.noreply.github.com>
2021-03-14 17:06:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
   packages = ['discord-ext-checks'],
   version = '0.1', 
   license = 'MIT',
-  description = 'A module which implements various new decorators to check for certain conditions in your discord commands',   # Give a short description about your library
-  author = 'multi-yt76',                   # Type in your name
-  author_email = '76multi@gmail.com',      # Type in your E-Mail
+  description = 'A module which implements various new decorators to check for certain conditions in your discord commands',
+  author = 'multi-yt76',
+  author_email = '76multi@gmail.com',
   url = 'https://github.com/multi-yt76/discord-ext-checks',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/multi-yt76/discord-ext-checks/archive/v0.1.tar.gz',
   keywords = ['discord', 'check', 'functions'],
   install_requires=[            # I get to this in a second
           'validators',
